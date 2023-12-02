b5971a1c00e703a502f2a9268f06f3e6256c5d42
Maks Sydoruk <maxsidoruk656@gmail.com>
2023-12-02 13:29:00 +0200

    apply changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import {VideoList } from './components/VideoList';
 import './App.css';
 
 function App() {
   return (
-     <h1>Hello.</h1>
+    <>
+       <h1>Hello.</h1>
+     <VideoList/>
         
+    </>
+    
           
   );
 }
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,5 +1,38 @@
+const videos = [
+    {
+    name: 'just video',
+    duration: '1 minute',
+    id: '1'
+},
+{
+    name: 'face reveal',
+    duration: '1 minute',
+    id: '2'
+},
+{
+    name: 'maybe video',
+    duration: '1 minute',
+    id: '3'
+}
+]
 export function VideoList(){
     return(
+        <>
         <h1>Video list</h1>
+        {
+            videos.map((video) => {
+                return (
+                    <div key={video.id}>
+                      <p>{video.name}</p>
+                      <p>{video.duration}</p>
+                      <button>Like!</button>
+                    </div>
+                )
+            })
+        }
+        
+        
+        </>
     )
+        
 }
